b5c75aa37d199aab25c5613e26e539b3eafec53d
Serat0 <r0746416@student.thomasmore.be>
2020-03-31 11:44:05 +0200

    advertisement working

--- Lab4-APIs/app.js
+++ Lab4-APIs/app.js
@@ -2,7 +2,7 @@ class Weather {
     constructor(){
         this.getLocation();
         this.latitude;
-        this.longitude;
+        this.longitude;        
     }
     
     getLocation(){
@@ -20,22 +20,23 @@ class Weather {
     }
 
     getWeather(){
+
         let url = `https://cors-anywhere.herokuapp.com/https://api.darksky.net/forecast/c25b51be5ae38894fa4e9acdef968a63/51.009945599999995,4.6989312?units=si`;
         fetch(url)
             .then(response => {
                 return response.json();
             })
             .then(data => {
-                document.querySelector(".summary").innerHTML = 
-                    data.currently.summary;
                 document.querySelector(".temp").innerHTML = 
                     data.currently.temperature + " Degrees";
+
                 
                     if(data.currently.temperature > 15){
                         document.getElementById("advertisement").src = "https://cdn.discordapp.com/attachments/614375570251317248/692716301491372082/JupilerAd.jpg";
                     } else {
                         document.getElementById("advertisement").src = "https://cdn.discordapp.com/attachments/614375570251317248/692716329282568272/StarbucksAd.png";
                     }
+
             })
             .catch(err=> {
                 console.log(err);
